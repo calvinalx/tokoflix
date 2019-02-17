5fb6fcd9a8d0b0df4823fb657230bb8b224da579
Calvin Alexander <calvinalx@gmail.com>
2019-02-18 03:25:53 +0700

    Ticket Purchasing functionality

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -10,7 +10,11 @@ class Movie extends Component {
 
     this.state = {
       id: null,
-      data: null
+      movie: null,
+      casts: null,
+      similar: null,
+      recommendations: null,
+      balance: null
     }
 
     this.TmdbApi = new TmdbApi('22e2f8fdb2d0ca58aa47ab6c7b2a6cb9')
@@ -19,34 +23,97 @@ class Movie extends Component {
   componentDidMount() {
     const id = this.props.location.state.id
     this.setState ({ id })
-    this.TmdbApi.fetchMovieDetails(id).then(data => this.setState({ data }))
+    this.TmdbApi.fetchMovieDetails(id).then(data => this.setState({ 
+      movie: data
+    }))
+    this.TmdbApi.fetchMovieCasts(id).then(data => this.setState({ 
+      casts: data
+    }))
+    this.TmdbApi.fetchSimilarMovies(id).then(data => this.setState({ 
+      similar: data
+    }))
+    this.TmdbApi.fetchSimilarMovies(id).then(data => this.setState({ 
+      recommendations: data
+    }))
+  }
+
+  buyTicket() {
+    let balance = parseInt(window.localStorage.getItem('balance'))
+    let price = priceCheck(this.state.movie.vote_average)
+    if (balance < price) {
+      alert('Saldo tidak mencukupi')
+    } else {
+      let newBalance = balance - price
+      window.localStorage.setItem(this.state.id, 'true')
+      window.localStorage.setItem('balance', newBalance)
+      this.setState({
+        balance
+      })
+      alert('Tiket telah dibeli') 
+    }
   }
 
   render() {
-    const { data } = this.state
+    const { movie, casts, similar, recommendations } = this.state
 
-    if (!data) {
+    if (!movie || !casts || !similar || !recommendations) {
       return(
-        <p>Loading...</p>
+        <div>
+          <Header />
+          <p className="text-center">Loading...</p>
+        </div>
       )
     }
 
     return (
       <div>
         <Header />
-        {console.log(this.state.data)}
-        <div className="container">
+        <div className="container"><br /><br />
           <div className="row">
             <div className="col-lg-4">
               <Poster 
-                id={data.id}
-                title={data.title}
-                poster={data.poster_path}
-                price={priceCheck(data.vote_average)} />
+                id={movie.id}
+                title={movie.title}
+                poster={movie.poster_path}
+                price={priceCheck(movie.vote_average)} />
             </div>
             <div className="col-lg-4">
-              Rating: {data.vote_average}<br />
-              Durasi: {data.runtime} menit<br />
+              <h2>{movie.title}</h2>
+              Rating: {movie.vote_average}<br />
+              Durasi: {movie.runtime} menit<br />
+              Casts: {casts.data.cast.map(cast => {
+                return (
+                  <span key={cast.id}>{cast.name}. </span>
+                )
+              })}<br /><br />
+              <h3>Rp {priceCheck(movie.vote_average)}</h3>
+              {(window.localStorage.getItem(this.state.id)) ?
+                <button className="btn btn-secondary" disabled>
+                  Sudah dibeli
+                </button>
+                :
+                <button 
+                  onClick={() => this.buyTicket()} 
+                  className="btn btn-primary">
+                  Beli Tiket
+                </button>
+              }
+            </div>
+            <div className="col-lg-4">
+              <h5>Similar movie</h5>
+              {similar.data.total_results < 1 ? 'Tidak ada film yang mirip' : ''}
+              {similar.data.results.map(movie => {
+                return (
+                  <span key={movie.id}>{movie.title}. </span>
+                )
+              })}<br /><br />
+              <h5>Rekomendasi film</h5>
+              {recommendations.data.total_results < 1 ? 'Tidak ada rekomendasi' : ''}
+              {recommendations.data.results.map(movie => {
+                return (
+                  <span key={movie.id}>{movie.title}. </span>
+                )
+              })}
             </div>
           </div>
         </div>
--- src/libs/TmdbApi.js
+++ src/libs/TmdbApi.js
@@ -14,6 +14,18 @@ class TmdbApi {
     return axios.get(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=${this.API_KEY}&language=en-US`)
       .then(res => res.data)
   }
+
+  fetchMovieCasts(movie_id) {
+    return axios.get(`https://api.themoviedb.org/3/movie/${movie_id}/credits?api_key=${this.API_KEY}`)
+  }
+
+  fetchSimilarMovies(movie_id) {
+    return axios.get(`https://api.themoviedb.org/3/movie/${movie_id}/similar?api_key=${this.API_KEY}&language=en-US&page=1`)
+  }
+
+  fetchRecommendations(movie_id) {
+    return axios.get(`https://api.themoviedb.org/3/movie/${movie_id}/recommendations?api_key=${this.API_KEY}&language=en-US&page=1`)
+  }
 }
 
 export default TmdbApi
